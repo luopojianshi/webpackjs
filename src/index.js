8b1c45b519776a76b13472aef6a0454d25be0c18
lijiafu <lijiafu@backustech.com>
2019-04-12 14:04:56 +0800

    webpack 中文网 指南--管理资源--加载数据

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
+import Data from './data.xml'
 
 function component () {
 	var element = document.createElement('div')
@@ -15,6 +16,8 @@ function component () {
 	// 将图像添加到我们现有的 div
 	var myIcon = new Image()
 	myIcon.src = Icon
+
+	console.log(Data)
 
 	element.appendChild(myIcon)
 
