27e3749addf4bd73a6f5792572eb30c00c6b44b5
luopojianshi <luopojianshi@163.com>
2019-04-16 01:17:27 +0800

    webpack 中文网 指南--模块热替换--启用 HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
 	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
+		app: './src/index.js'
 	},
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: './dist'
+		contentBase: './dist',
+		hot: true
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Output Management 管理输出'
-		})
+		}),
+		new webpack.NamedModulesPlugin(),
+		// new webpack.HotModuleReplacementPlugin()
 	],
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/'
+		path: path.resolve(__dirname, 'dist')
 	},
 	performance: {
 		hints: false
