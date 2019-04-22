aa7bb26450734d9690fc7810bcce60480ae44a4c
lijiafu <lijiafu@backustech.com>
2019-04-22 12:54:33 +0800

    webpack 中文网 指南--代码分离--入口起点

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import { cube } from './math.js'
-
-if (process.env.NODE_ENV !== 'production') {
-	console.log('Looks like we are in development mode!')
-}
+import _ from 'lodash'
+import printMe from './print.js'
 
 function component () {
-	var element = document.createElement('pre')
-	
-	element.innerHTML = [
-		'Hello webpack!',
-		'5 cubed is equal to ' + cube(5)
-	].join('\n\n')
+	var element = document.createElement('div')
+	var btn = document.createElement('button')
+
+	// Lodash, now imported by this script
+	element.innerHTML = _.join(['Hello', 'webpack!'], ' ')
+
+	btn.innerHTML = 'Click me and check the console!'
+	btn.onclick = printMe
+
+	element.appendChild(btn)
 
 	return element
 }
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: {
+		index: './src/index.js',
+		another: './src/another-module.js'
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			title: 'Code Splitting 代码分离'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
+
