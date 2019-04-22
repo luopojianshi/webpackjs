f9cdd883044488dd073684a4dbbba4921530701a
luopojianshi <luopojianshi@163.com>
2019-04-23 00:17:27 +0800

    webpack 中文网 指南--代码分离--防止重复

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -11,6 +12,17 @@ module.exports = {
 			title: 'Code Splitting 代码分离'
 		})
 	],
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					name: 'commons',
+					chunks: 'initial',
+					minChunks: 2
+				}
+			}
+		}
+	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
